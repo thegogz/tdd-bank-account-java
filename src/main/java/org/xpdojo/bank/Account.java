d1c1ee6510b13e79e6a303f1dec678b57ae5e4e7
Eoin O'Neill <thegogz@gmail.com>
2020-10-06 14:39:55 +0100

    Adding withdraw methods and tests

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -4,12 +4,21 @@ public class Account {
 
     private int balance = 0;
 
+    public Account(int startingBalance) {
+        balance = startingBalance;
+    }
+
     public int balance() {
         return balance;
     }
 
     public int deposit(int amount) {
         balance += amount;
+        return balance;
+    }
+
+    public int withdraw(int amount) {
+        balance -= amount;
         return balance;
     }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -9,22 +9,38 @@ public class AccountTest {
 
     @Test
     public void newAccountShouldHaveZeroBalance() {
-        Account a = new Account();
+        Account a = new Account(0);
         assertThat(a.balance()).isEqualTo(0);
     }
 
     @Test
     public void depositingAmountShouldIncreaseBalance() {
-        Account account = new Account();
+        Account account = new Account(0);
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(10);
     }
 
     @Test
     public void multipleDepositsShouldIncreaseTheBalance() {
-        Account account = new Account();
+        Account account = new Account(0);
         account.deposit(10);
         account.deposit(10);
         assertThat(account.balance()).isEqualTo(20);
     }
+
+    @Test
+    public void withdrawAmountShouldDecreaseTheBalance() {
+        Account account = new Account(10);
+        account.withdraw(10);
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
+    @Test
+    public void withdrawMultipleShouldDecreaseTheBalance() {
+        Account account = new Account(20);
+        account.withdraw(10);
+        account.withdraw(10);
+        assertThat(account.balance()).isEqualTo(0);
+    }
+
 }
