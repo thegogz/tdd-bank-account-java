6553dca557f7017ba990c923ae1b7364d5d74e06
Eoin O'Neill <thegogz@gmail.com>
2020-10-06 14:06:09 +0100

    Adding first test

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,9 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+
+    public int balance() {
+        return 0;
+    }
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -8,8 +8,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void accountBalanceShouldBeZero() {
+        Account a = new Account();
+        assertThat(a.balance()).isEqualTo(0);
     }
 }
